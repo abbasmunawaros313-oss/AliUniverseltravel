1dc574bc0b1966ff6cb94bc6a5d0666f93effa7b
abbasmunawaros313-oss <abbasmunawaros313@gmail.com>
2025-09-20 10:05:44 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAMOTHc9U1whgepO8AAE0-xaK7xiNjqqVQ",
-  authDomain: "osportalmanagmnet.firebaseapp.com",
-  projectId: "osportalmanagmnet",
-  storageBucket: "osportalmanagmnet.firebasestorage.app",
-  messagingSenderId: "846394060555",
-  appId: "1:846394060555:web:a4c6d3b64af58cd77f3c0e"
+  apiKey: "AIzaSyDGZirPFQMP630cUew09mOv5dNb_pp8p_o",
+  authDomain: "aliuniverseltravels.firebaseapp.com",
+  projectId: "aliuniverseltravels",
+  storageBucket: "aliuniverseltravels.firebasestorage.app",
+  messagingSenderId: "507341726614",
+  appId: "1:507341726614:web:092e73a20cb3448af0b3b7",
+  measurementId: "G-MLRS6T3LDE"
 };
 
 // Initialize Firebase
